1bfeccdb2da98090a60ddfa6dfce5c8b02092e26
Joe Fryer <joe.d.fryer@gmail.com>
2013-08-22 23:03:29 +0100

    Initial commit

---
+++ JDFToolkit/JDFToolkit/JDFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDFAppDelegate.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 22/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JDFToolkit/JDFToolkit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 22/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDFAppDelegate class]));
+    }
+}
