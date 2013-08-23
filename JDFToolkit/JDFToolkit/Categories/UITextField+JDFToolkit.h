d3467f62a800da3b5829889ffc69af2d4ce82bea
Joe Fryer <joe.d.fryer@gmail.com>
2013-08-23 08:02:41 +0100

    Added UITextField+JDFToolkit.

---
+++ JDFToolkit/JDFToolkit/Categories/UITextField+JDFToolkit.h
@@ -0,0 +1,16 @@
+//
+//  UITextField+JDFToolkit.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 23/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (JDFToolkit)
+
+- (void)configureAsEmailField;
+- (void)configureAsNameEntryField;
+
+@end
---
+++ JDFToolkit/JDFToolkit/Categories/UITextField+JDFToolkit.m
@@ -0,0 +1,25 @@
+//
+//  UITextField+JDFToolkit.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 23/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import "UITextField+JDFToolkit.h"
+
+@implementation UITextField (JDFToolkit)
+
+- (void)configureAsEmailField
+{
+    self.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.keyboardType = UIKeyboardTypeEmailAddress;
+}
+
+- (void)configureAsNameEntryField
+{
+    self.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.autocapitalizationType = UITextAutocapitalizationTypeWords;
+}
+
+@end
