a5bf9e9fda5b9b85655362c8054130a18c88ebdb
Joe Fryer <joe@qaplus.co.uk>
2013-08-28 17:09:27 +0100

    Added JDFProjectUtilities.

---
+++ JDFToolkit/JDFToolkit/JDFProjectUtilities.h
@@ -0,0 +1,13 @@
+//
+//  JDFProjectUtilities.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 28/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDFProjectUtilities : NSObject
+
+@end
