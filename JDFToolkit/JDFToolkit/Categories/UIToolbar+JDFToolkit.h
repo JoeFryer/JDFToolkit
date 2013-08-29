60c38f27c34ad2be75be48eacecac99d50dd4e67
Joe Fryer <joe@qaplus.co.uk>
2013-08-29 11:49:56 +0100

    Added UIToolbar+JDFToolkit.

---
+++ JDFToolkit/JDFToolkit/Categories/UIToolbar+JDFToolkit.h
@@ -0,0 +1,13 @@
+//
+//  UIToolbar+JDFToolkit.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 29/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIToolbar (JDFToolkit)
+
+@end
---
+++ JDFToolkit/JDFToolkit/Categories/UIToolbar+JDFToolkit.m
@@ -0,0 +1,13 @@
+//
+//  UIToolbar+JDFToolkit.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 29/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import "UIToolbar+JDFToolkit.h"
+
+@implementation UIToolbar (JDFToolkit)
+
+@end
