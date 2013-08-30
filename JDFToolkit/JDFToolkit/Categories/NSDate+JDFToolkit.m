ecc54c7a30da190f7356ab13f1da48a515db9d1d
Joe Fryer <joe.d.fryer@gmail.com>
2013-08-31 00:28:29 +0100

    Added NSDate+JDFToolkit.

---
+++ JDFToolkit/JDFToolkit/Categories/NSDate+JDFToolkit.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+JDFToolkit.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 31/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import "NSDate+JDFToolkit.h"
+
+@implementation NSDate (JDFToolkit)
+
+@end
