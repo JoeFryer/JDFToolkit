9c5c395642d7995a10c98ffac69f5c5bd306a44f
Joe Fryer <joe.d.fryer@gmail.com>
2013-08-31 22:42:24 +0100

    Added JDFDateFormatterCache.

---
+++ JDFToolkit/JDFToolkit/Classes/JDFDateFormatterCache.h
@@ -0,0 +1,13 @@
+//
+//  JDFDateFormatterCache.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 31/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDFDateFormatterCache : NSObject
+
+@end
---
+++ JDFToolkit/JDFToolkit/Classes/JDFDateFormatterCache.m
@@ -0,0 +1,13 @@
+//
+//  JDFDateFormatterCache.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 31/08/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import "JDFDateFormatterCache.h"
+
+@implementation JDFDateFormatterCache
+
+@end
