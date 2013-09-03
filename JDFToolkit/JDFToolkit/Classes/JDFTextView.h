d53cfecf7f9f2ea9981e4906d8a8e8ad49d815ae
Joe Fryer <joe@qaplus.co.uk>
2013-09-03 16:58:56 +0100

    Added JDFTextView.

---
+++ JDFToolkit/JDFToolkit/Classes/JDFTextView.h
@@ -0,0 +1,16 @@
+//
+//  JDFTextView.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 03/09/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDFTextView : UITextView
+
+@property (nonatomic, strong) NSString *placeholder;
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+@end
---
+++ JDFToolkit/JDFToolkit/Classes/JDFTextView.m
@@ -0,0 +1,115 @@
+//
+//  JDFTextView.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 03/09/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import "JDFTextView.h"
+
+
+@interface JDFTextView()
+
+@property (nonatomic) BOOL placeholderIsVisible;
+@property (nonatomic, strong) UIColor *standardTextColor;
+
+@end
+
+
+
+@implementation JDFTextView
+
+
+#pragma mark - Setters
+
+/*
+ We do not use this setter internally - we call [super setTextColor:] when we want to change the text color. This setter is to intersect the user setting a text color externally, in which case we store their desired color in self.standardTextColor (however, we only set this via super here if the placeholder is not visible, as if it is visible we don't want to change it as we want the text to have the placeholderColor!).
+ */
+- (void)setTextColor:(UIColor *)textColor
+{
+    self.standardTextColor = textColor;
+    if (!self.placeholderIsVisible) {
+        [super setTextColor:textColor];
+    }
+}
+
+- (void)setPlaceholder:(NSString *)placeholder
+{
+    _placeholder = placeholder;
+    if ([self.text isEqualToString:@""]) {
+        self.text = self.placeholder;
+        self.placeholderIsVisible = YES;
+    }
+}
+
+- (void)setPlaceholderIsVisible:(BOOL)placeholderIsVisible
+{
+    // Here, when we set our text color, we set it on super, as we have overriden setTextColor: to intersect a text color change that the user makes and store it in self.standardTextColor (but we don't want to do use that internally as we will end up making self.standardTextColor the same as our placeholderColor).
+    _placeholderIsVisible = placeholderIsVisible;
+    if (_placeholderIsVisible) {
+        [super setTextColor:self.placeholderColor];
+    } else {
+        [super setTextColor:self.standardTextColor];
+    }
+}
+
+
+#pragma mark - Notification Receivers
+
+- (void)textDidBeginEditing:(NSNotification *)notification
+{
+    if (self.placeholderIsVisible) {
+        self.text = @"";
+        self.placeholderIsVisible = NO;
+    }
+}
+
+- (void)textDidEndEditing:(NSNotification *)notification
+{
+    if ([self.text isEqualToString:@""]) {
+        self.text = self.placeholder;
+        self.placeholderIsVisible = YES;
+    }
+}
+
+
+#pragma mark - Lifecycle
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self customInit];
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [self customInit];
+}
+
+- (void)customInit
+{
+    self.standardTextColor = [UIColor colorWithCGColor:self.textColor.CGColor];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidBeginEditing:) name:UITextViewTextDidBeginEditingNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidEndEditing:) name:UITextViewTextDidEndEditingNotification object:nil];
+    
+    if (!self.placeholder) {
+        self.placeholder = @"";
+    }
+    if (!self.placeholderColor) {
+        self.placeholderColor = [UIColor grayColor];
+    }
+    
+    if ([self.text isEqualToString:@""]) {
+        self.text = self.placeholder;
+        self.placeholderIsVisible = YES;
+    } else {
+        self.placeholderIsVisible = NO;
+    }
+}
+
+@end
