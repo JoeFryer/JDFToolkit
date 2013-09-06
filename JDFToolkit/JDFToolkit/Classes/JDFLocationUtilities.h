ebe3ed4cd0167e7d3389c214499de0f08c0f4fe9
Joe Fryer <joe@qaplus.co.uk>
2013-09-06 17:05:54 +0100

    Added JDFLocationUtilities.

---
+++ JDFToolkit/JDFToolkit/Classes/JDFLocationUtilities.h
@@ -0,0 +1,13 @@
+//
+//  JDFLocationUtilities.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 06/09/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDFLocationUtilities : NSObject
+
+@end
---
+++ JDFToolkit/JDFToolkit/Classes/JDFLocationUtilities.m
@@ -0,0 +1,13 @@
+//
+//  JDFLocationUtilities.m
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 06/09/2013.
+//  Copyright (c) 2013 JDF. All rights reserved.
+//
+
+#import "JDFLocationUtilities.h"
+
+@implementation JDFLocationUtilities
+
+@end
