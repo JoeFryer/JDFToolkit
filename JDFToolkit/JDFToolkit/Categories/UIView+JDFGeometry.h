9de9e0ba73aee44968ecf2cdfde62231a7e8d2fa
Joe Fryer <joe@qaplus.co.uk>
2014-01-08 10:23:48 +0000

    Added some Convenience Accessors and frame manipulation methods.

--- JDFToolkit/JDFToolkit/Categories/UIView+JDFGeometry.h
+++ JDFToolkit/JDFToolkit/Categories/UIView+JDFGeometry.h
@@ -10,6 +10,30 @@
 
 @interface UIView (JDFGeometry)
 
-- (void)centerXInSuperview;
+#pragma mark - Convenience Accessors
+
+- (CGFloat)frameX;
+- (CGFloat)frameY;
+- (CGFloat)frameWidth;
+- (CGFloat)frameHeight;
+
+- (void)setFrameX:(CGFloat)frameX;
+- (void)setFrameY:(CGFloat)frameY;
+- (void)setFrameWidth:(CGFloat)frameWidth;
+- (void)setFrameHeight:(CGFloat)frameHeight;
+
+#pragma mark - Convenience frame manipulation
+
+- (void)centerXInSuperview __attribute__ ((deprecated));
+- (void)centerYInSuperview __attribute__ ((deprecated));
+
+- (void)centerInSuperview;
+- (void)centerHorizontallyInSuperview;
+- (void)centerVerticallyInSuperview;
+
+- (void)moveViewXBy:(CGFloat)xDelta;
+- (void)moveViewYBy:(CGFloat)yDelta;
+- (void)changeViewWidthBy:(CGFloat)widthDelta;
+- (void)changeViewHeightBy:(CGFloat)heightDelta;
 
 @end
--- JDFToolkit/JDFToolkit/Categories/UIView+JDFGeometry.m
+++ JDFToolkit/JDFToolkit/Categories/UIView+JDFGeometry.m
@@ -10,7 +10,78 @@
 
 @implementation UIView (JDFGeometry)
 
+#pragma mark - Convenience Accessors
+
+- (CGFloat)frameX
+{
+    return self.frame.origin.x;
+}
+
+- (CGFloat)frameY
+{
+    return self.frame.origin.y;
+}
+
+- (CGFloat)frameWidth
+{
+    return self.frame.size.width;
+}
+
+- (CGFloat)frameHeight
+{
+    return self.frame.size.height;
+}
+
+- (void)setFrameX:(CGFloat)frameX
+{
+    CGRect frame = self.frame;
+    frame.origin.x = frameX;
+    self.frame = frame;
+}
+
+- (void)setFrameY:(CGFloat)frameY
+{
+    CGRect frame = self.frame;
+    frame.origin.y = frameY;
+    self.frame = frame;
+}
+
+- (void)setFrameWidth:(CGFloat)frameWidth
+{
+    CGRect frame = self.frame;
+    frame.size.width = frameWidth;
+    self.frame = frame;
+}
+
+- (void)setFrameHeight:(CGFloat)frameHeight
+{
+    CGRect frame = self.frame;
+    frame.size.height = frameHeight;
+    self.frame = frame;
+}
+
+
+#pragma mark - Convenience frame manipulation
+
+/* Deprecated */
 - (void)centerXInSuperview
+{
+    [self centerHorizontallyInSuperview];
+}
+
+/* Deprecated */
+- (void)centerYInSuperview
+{
+    [self centerVerticallyInSuperview];
+}
+
+- (void)centerInSuperview
+{
+    [self centerHorizontallyInSuperview];
+    [self centerVerticallyInSuperview];
+}
+
+- (void)centerHorizontallyInSuperview
 {
     CGFloat viewWidth = self.frame.size.width;
     CGFloat superviewWidth = self.superview.frame.size.width;
@@ -20,7 +91,7 @@
     self.frame = frame;
 }
 
-- (void)centerYInSuperview
+- (void)centerVerticallyInSuperview
 {
     CGFloat viewHeight = self.frame.size.height;
     CGFloat superviewHeight = self.superview.frame.size.height;
@@ -29,5 +100,35 @@
     frame.origin.y = (superviewHeight - viewHeight) / 2;
     self.frame = frame;
 }
+
+- (void)moveViewXBy:(CGFloat)xDelta
+{
+    CGRect frame = self.frame;
+    frame.origin.x += xDelta;
+    self.frame = frame;
+}
+
+- (void)moveViewYBy:(CGFloat)yDelta
+{
+    CGRect frame = self.frame;
+    frame.origin.y += yDelta;
+    self.frame = frame;
+}
+
+- (void)changeViewWidthBy:(CGFloat)widthDelta
+{
+    CGRect frame = self.frame;
+    frame.size.width += widthDelta;
+    self.frame = frame;
+}
+
+- (void)changeViewHeightBy:(CGFloat)heightDelta
+{
+    CGRect frame = self.frame;
+    frame.size.height += heightDelta;
+    self.frame = frame;
+}
+
+
 
 @end
