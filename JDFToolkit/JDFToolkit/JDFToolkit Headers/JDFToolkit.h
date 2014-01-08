d2fa9219e23c007908e9066a0b0af00551f3c675
Joe Fryer <joe@qaplus.co.uk>
2014-01-08 12:59:52 +0000

    Added JDFToolkit & JDFToolkitCategories header files.

---
+++ JDFToolkit/JDFToolkit/JDFToolkit Headers/JDFToolkit.h
@@ -0,0 +1,14 @@
+//
+//  JDFToolkit.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 08/01/2014.
+//  Copyright (c) 2014 JDF. All rights reserved.
+//
+
+#import "JDFProjectUtilities.h"
+#import "JDFDateFormatterCache.h"
+#import "JDFLocationUtilities.m"
+#import "JDFTableItem.m"
+#import "JDFTableSection.m"
+#import "JDFTextView.m"
---
+++ JDFToolkit/JDFToolkit/JDFToolkitCategories.h
@@ -0,0 +1,15 @@
+//
+//  JDFToolkitCategories.h
+//  JDFToolkit
+//
+//  Created by Joe Fryer on 08/01/2014.
+//  Copyright (c) 2014 JDF. All rights reserved.
+//
+
+#import "NSDate+JDFToolkit.h"
+#import "NSObject+JDFBlocks.h"
+#import "NSString+JDFToolkit.h"
+#import "UILabel+JDFToolkit.h"
+#import "UITextField+JDFToolkit.h"
+#import "UIToolbar+JDFToolkit.h"
+#import "UIView+JDFGeometry.h"
